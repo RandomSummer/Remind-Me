695780239b5c1724ff9c63c035e4588a906e4ce4
RandomSummer <sksofiqueefiaz2015@gmail.com>
2025-09-01 00:44:05 +0530

    adding functionality to the website like toggle, delete

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -4,10 +4,7 @@ import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,18 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title) {
         taskServices.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskServices.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskServices.toggleTask(id);
         return "redirect:/";
     }
 }
